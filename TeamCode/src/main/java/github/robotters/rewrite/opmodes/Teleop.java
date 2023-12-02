6bbe8005bb9673a174d8e337c1f69df6d0161ccd
foehammer127 <foehammer@disroot.org>
2023-12-01 23:31:20 -0600

    feat(opmodes): add teleop

--- TeamCode/src/main/java/github/robotters/rewrite/opmodes/Teleop.java
+++ TeamCode/src/main/java/github/robotters/rewrite/opmodes/Teleop.java
@@ -1,2 +1,28 @@
-package github.robotters.rewrite.opmodes;public class Teleop {
+package github.robotters.rewrite.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import github.robotters.rewrite.Robot;
+import github.robotters.rewrite.RobotProps;
+import github.robotters.rewrite.RobotState;
+
+@TeleOp
+public class Teleop extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotProps props = new RobotProps(hardwareMap, gamepad1);
+
+        Robot r = new Robot(props);
+        r.teleopInit();
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            r.run();
+            RobotState.getInstance().outputtoTelemetry(telemetry);
+            telemetry.update();
+        }
+
+        r.reset();
+    }
 }
